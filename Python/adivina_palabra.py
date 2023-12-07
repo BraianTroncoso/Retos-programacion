2659f845fbe7853abc4f8eafdd3535b302095fd3
Braian <troncosobraianaxel@gmail.com>
2023-12-07 01:49:53 -0300

    [ADD] ahorcado en python

--- Python/adivina_palabra.py
+++ Python/adivina_palabra.py
@@ -11,3 +11,43 @@ for index, letter in enumerate(word):
     hidden_word += "_" if index in hidden_positions else letter
 
     
+attempts = 5
+
+while attempts > 0:
+    
+    print(f'Adivina la palabra: {hidden_word}\nTienes {attempts} intentos.')
+
+    text = input('Introduce una letra o la solución completa: ')
+
+    if len(text) == 1:
+
+        new_hidden_word = ""
+        succes = False
+        for index, letter in enumerate(word):
+            if text == letter and hidden_word[index] == "_":
+                new_hidden_word += text
+                succes = True
+            else:
+                new_hidden_word += hidden_word[index]
+        hidden_word = new_hidden_word
+
+        if succes:
+            if word == hidden_word:
+                print(f'¡Has acertado! La palabra oculta era: {word}')
+                break
+            else:
+                print('Has acertado la letra!')
+        else:
+            print('Letra no encontrada.')
+            attempts -= 1
+    elif len(text) == len(word):
+        if text == word:
+            print(f'¡Has acertado! La palabra oculta era: {word}')
+            break
+        else:
+            print('La palabra no es correcta.')  
+            attempts -= 1  
+    else:
+        ('Texto invalido.')
+if attempts == 0:
+    print(f'Has perdido. La palabra correcta era {word}.')        
