bcf7684b1e0a8b3a30274aec5cacb4471ad4e0e1
Braian <troncosobraianaxel@gmail.com>
2024-01-04 00:57:05 -0300

    [ADD] Java - Clean code -Realizar distinciones con sentido

--- Java/CleanCode/CleanCode.java
+++ Java/CleanCode/CleanCode.java
@@ -4,13 +4,30 @@ import java.util.List;
 public class CleanCode{
     public static void main(String[] args) {
 
-        // Usar nombres que revelen las intenciones
-        public List<Cell> getFlaggedCells() {
-            List<Cell> flaggedCells = new ArrayList<Cell>();
-            for (Cell cell : gameBoard)
-            if (cell.isFlagged())
-            flaggedCells.add(cell);
-            return flaggedCells;
-            }
+        // Usar nombres que revelen las intenciones 1)
+
+        // public List<Cell> getFlaggedCells() {
+        //     List<Cell> flaggedCells = new ArrayList<Cell>();
+        //     for (Cell cell : gameBoard)
+        //     if (cell.isFlagged())
+        //     flaggedCells.add(cell);
+        //     return flaggedCells;
+        //     }
+
+        // Realizar distinciones con sentido 2)
+
+        // public static void copyChars(char a1[], char a2[]) {
+        // for (int i = 0; i < a1.length; i++) {
+        //         a2[i] = a1[i];
+        //     }
+        // }
+        // Los nombres tienen que ser mas descriptivos y tienen que ser notablemente diferentes
+        // Aca abajo ejemplo de como NO se debe hacer
+        //     getActiveAccount();
+        //     getActiveAccounts();
+        //     getActiveAccountInfo();
+
+        
+    
     }
 }
