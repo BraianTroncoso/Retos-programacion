f448c4ce0198fa3c1dfc0c8121ad90df5ebc32c9
Braian <troncosobraianaxel@gmail.com>
2024-01-04 01:03:43 -0300

    [ADD] Java - Clean code - Usar nombres que se puedan pronunciar

--- Java/CleanCode/CleanCode.java
+++ Java/CleanCode/CleanCode.java
@@ -16,18 +16,34 @@ public class CleanCode{
 
         // Realizar distinciones con sentido 2)
 
+        // Los nombres tienen que ser mas descriptivos y tienen que ser notablemente diferentes
+        // Aca abajo ejemplo de como NO se debe hacer
         // public static void copyChars(char a1[], char a2[]) {
         // for (int i = 0; i < a1.length; i++) {
         //         a2[i] = a1[i];
         //     }
         // }
-        // Los nombres tienen que ser mas descriptivos y tienen que ser notablemente diferentes
-        // Aca abajo ejemplo de como NO se debe hacer
         //     getActiveAccount();
         //     getActiveAccounts();
         //     getActiveAccountInfo();
 
-        
+        // Usar nombres que se puedan pronunciar 3)
+
+        // Compare:
+        // class DtaRcrd102 {
+        // private Date genymdhms;
+        // private Date modymdhms;
+        // private final String pszqint = “102”;
+        // /*… */
+        // };
+        // con:
+        // class Customer {
+        // private Date generationTimestamp;
+        // private Date modificationTimestamp;
+        // private final String recordId = “102”;
+        // /*… */
+        // };
+            
     
     }
 }
