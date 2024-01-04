5334fdb71fcae26c9172a889d542438a4fca676a
Braian <troncosobraianaxel@gmail.com>
2024-01-04 01:09:21 -0300

    [ADD] Java - Clean code - Usar nombres que se puedan buscar

--- Java/CleanCode/CleanCode.java
+++ Java/CleanCode/CleanCode.java
@@ -43,7 +43,23 @@ public class CleanCode{
         // private final String recordId = “102”;
         // /*… */
         // };
+
+
+        // Usar nombres que se puedan buscar
+
+        // Compare:
+        // for (int j=0; j<34; j++) {
+        // s += (t[j]*4)/5;
+        // }
+        // con:
+        // int realDaysPerIdealDay = 4;
+        // const int WORK_DAYS_PER_WEEK = 5;
+        // int sum = 0;
+        // for (int j = 0; j < NUMBER_OF_TASKS; j++) {
+        // int realTaskDays = taskEstimate[j] * realDaysPerIdealDay;
+        // int realTaskWeeks = (realdays / WORK_DAYS_PER_WEEK);
+        // sum += realTaskWeeks;
+}
             
     
     }
-}
